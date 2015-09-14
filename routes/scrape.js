19c5357e20cc4c4ac90e08583402c17bfd0d29f9
gejose <george.jose.a@gmail.com>
2015-09-14 19:26:51 -0400

    Implemented currency conversion

--- routes/scrape.js
+++ routes/scrape.js
@@ -11,14 +11,30 @@ router.get('/', function(req, res, next) {
         console.log('Attempting to get page at: ' + fullUrl);
         request(fullUrl, function(error, response, html){
             if(!error){
+                var conversionRate = 1;
                 $ = cheerio.load(html);
                 var stockPrice = $('tbody tr td span b').first().text()
-                var stockSymbol = $('a b').first().text()
-                var change = $('td span cite').first().text()
-                var companyName = $('div h3 span').text().replace(' - ', '')
-                var lastUpdated = $('table tbody tr td div div table .f,ct-active').first().text()
-                var json = {"query": query, "companyName": companyName, "stockSymbol": stockSymbol, "lastUpdated": lastUpdated, "marketValue": stockPrice, "change": change};
-                res.send(json);
+                if(query.indexOf("NASDAQ")!=-1){
+                    request("http://api.fixer.io/latest?base=USD&symbols=CAD", function(e, r, h){
+                    conversionRate = JSON.parse(r.body)["rates"]["CAD"];
+                    console.log("conversion rate = " + conversionRate);
+                    stockPrice = stockPrice * parseFloat(conversionRate);
+                    var stockSymbol = $('a b').first().text()
+                    var change = $('td span cite').first().text()
+                    var companyName = $('div h3 span').text().replace(' - ', '')
+                    var lastUpdated = $('table tbody tr td div div table .f,ct-active').first().text()
+                    var json = {"query": query, "companyName": companyName, "stockSymbol": stockSymbol, "lastUpdated": lastUpdated, "marketValue": stockPrice, "change": change};
+                    res.send(json);
+                    });
+                }else{
+                    stockPrice = stockPrice * parseFloat(conversionRate);
+                    var stockSymbol = $('a b').first().text()
+                    var change = $('td span cite').first().text()
+                    var companyName = $('div h3 span').text().replace(' - ', '')
+                    var lastUpdated = $('table tbody tr td div div table .f,ct-active').first().text()
+                    var json = {"query": query, "companyName": companyName, "stockSymbol": stockSymbol, "lastUpdated": lastUpdated, "marketValue": stockPrice, "change": change};
+                    res.send(json);
+                }
             }
         });
     }
