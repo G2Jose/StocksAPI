a578db50abb4749cc20feacb032de2f64a010104
gejose <george.jose.a@gmail.com>
2015-09-15 21:52:34 -0400

    changed unit of 'change' to CAD

--- routes/scrape.js
+++ routes/scrape.js
@@ -21,10 +21,11 @@ router.get('/', function(req, res, next) {
                     conversionRate = JSON.parse(r.body)["rates"]["CAD"];
                     // console.log("conversion rate = " + conversionRate);
                     stockPrice = (stockPrice * parseFloat(conversionRate)).toString();
-                    var stockSymbol = $('a b').first().text()
-                    var change = $('td span cite').first().text()
-                    var companyName = $('div h3 span').text().replace(' - ', '')
-                    var lastUpdated = $('table tbody tr td div div table .f,ct-active').first().text()
+                    var stockSymbol = $('a b').first().text();
+                    var change = $('td span cite').first().text();
+                    var change = (parseFloat(change) * parseFloat(conversionRate)).toString();
+                    var companyName = $('div h3 span').text().replace(' - ', '');
+                    var lastUpdated = $('table tbody tr td div div table .f,ct-active').first().text();
                     var json = {"query": query, "companyName": companyName, "stockSymbol": stockSymbol, "lastUpdated": lastUpdated, "marketValue": stockPrice, "change": change};
                     // console.log(json);
                     res.send(json);
