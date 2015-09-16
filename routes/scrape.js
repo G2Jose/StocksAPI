22de117e0805cab3454cd0b586a9ec5429637de1
gejose <george.jose.a@gmail.com>
2015-09-15 22:10:37 -0400

    Fixed currency conversion for day's change

--- routes/scrape.js
+++ routes/scrape.js
@@ -40,6 +40,7 @@ router.get('/', function(req, res, next) {
                     var companyName = $('div h3 span').text().replace(' - ', '')
                     var lastUpdated = $('table tbody tr td div div table .f,ct-active').first().text()
                     var json = {"query": query, "companyName": companyName, "stockSymbol": stockSymbol, "lastUpdated": lastUpdated, "marketValue": stockPrice, "change": change};
+                    console.log(stockSymbol+'\tParsed change =\t'+change)
                     // console.log(json);
                     res.send(json);
                 }
